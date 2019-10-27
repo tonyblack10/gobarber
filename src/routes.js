7e83ecc98186448e66266cfdb42bd8e821656b76
Tony Costa <tonyblack10@gmail.com>
2019-10-27 02:13:14 -0300

    Cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Tony Costa',
-    email: 'tony@email.com',
-    password_hash: '12345442132',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
