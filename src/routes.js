819830000a97684eb42a6313a7ca93b16d32361e
Tony Costa <tonyblack10@gmail.com>
2019-12-01 01:27:55 -0300

    Avatar do usuário

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
