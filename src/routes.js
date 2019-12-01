cc5c5bddbdaa5dc27eb54fbe714f06da9af35a4c
Tony Costa <tonyblack10@gmail.com>
2019-12-01 03:14:56 -0300

    Agendamento de serviço

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,7 +17,9 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
+routes.post('/files', upload.single('file'), FileController.store);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
